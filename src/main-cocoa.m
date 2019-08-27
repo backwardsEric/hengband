backwardsEric/hengband

commit 9843a18bde263d35049e3a19056fbb4e8f980eba

Use the Angband global, angband_term_name, as the source for the terminal names so that they are consistent with what is shown by Angband's command to configure the contents of the terminals.

src/main-cocoa.m
@@ -1660,11 +1660,19 @@ static void Term_init_cocoa(term *t)
 
     /* Get the window */
     NSWindow *window = [context makePrimaryWindow];
-    
+    NSString *title;
+
     /* Set its title and, for auxiliary terms, tentative size */
     if (termIdx == 0)
     {
-        [window setTitle:@"Hengband"];
+	title = [NSString stringWithCString:angband_term_name[0]
+#ifdef JP
+			      encoding:NSJapaneseEUCStringEncoding
+#else
+			      encoding:NSMacOSRomanString
+#endif
+	];
+        [window setTitle:title];
 
         /* Set minimum size (80x24) */
         NSSize minsize;
@@ -1674,7 +1682,14 @@ static void Term_init_cocoa(term *t)
     }
     else
     {
-        [window setTitle:[NSString stringWithFormat:@"Term %d", termIdx]];
+	title = [NSString stringWithCString:angband_term_name[termIdx]
+#ifdef JP
+			      encoding:NSJapaneseEUCStringEncoding
+#else
+			      encoding:NSMacOSRomanString
+#endif
+	];
+        [window setTitle:title];
         /* Set minimum size (1x1) */
         NSSize minsize;
         minsize.width = context->tileSize.width + context->borderSize.width * 2.0;

commit 6facb371e70ba2cb91a50b07421c7cc2ffda90da

Corrections for compiler errors introduced by commit 9843a18bde263d35049e3a19056fbb4e8f980eba when the source had been configured with Japanese disabled.

src/main-cocoa.m
@@ -1697,7 +1697,7 @@ static void Term_init_cocoa(term *t)
 #ifdef JP
 			      encoding:NSJapaneseEUCStringEncoding
 #else
-			      encoding:NSMacOSRomanString
+			      encoding:NSMacOSRomanStringEncoding
 #endif
 	];
         [window setTitle:title];
@@ -1714,7 +1714,7 @@ static void Term_init_cocoa(term *t)
 #ifdef JP
 			      encoding:NSJapaneseEUCStringEncoding
 #else
-			      encoding:NSMacOSRomanString
+			      encoding:NSMacOSRomanStringEncoding
 #endif
 	];
         [window setTitle:title];