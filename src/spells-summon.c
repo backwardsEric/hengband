14d2a812f4e1b71c43d4f8a03ab625fa8a0296f0
Eric Branlund <ebranlund@fastmail.com>
2019-09-11 18:02:01 -0700

    Add missing "to" in English message from mitokohmon().

--- src/spells-summon.c
+++ src/spells-summon.c
@@ -422,7 +422,7 @@ void mitokohmon(void)
 	if (count)
 	{
 		msg_format(_("「者ども、ひかえおろう！！！このお方をどなたとこころえる。」",
-			"%^s says 'WHO do you think this person is! Bow your head, down your knees!'"), kakusan);
+			"%^s says 'WHO do you think this person is! Bow your head, down to your knees!'"), kakusan);
 		sukekaku = TRUE;
 		stun_monsters(120);
 		confuse_monsters(120);
