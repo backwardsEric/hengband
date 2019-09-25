f542772b159e7f275ee75a44e325211921f6007b
Eric Branlund <ebranlund@fastmail.com>
2019-09-24 17:40:00 -0700

    Changed "exceptionally" to "exceptional" in English message since it's used as an adjective.

--- src/bldg.c
+++ src/bldg.c
@@ -3148,7 +3148,7 @@ static PRICE repair_broken_weapon_aux(PRICE bcost)
 			one_ability(o_ptr);
 		}
 
-		msg_print(_("これはかなりの業物だったようだ。", "This blade seems to be exceptionally."));
+		msg_print(_("これはかなりの業物だったようだ。", "This blade seems to be exceptional."));
 	}
 
 	object_desc(basenm, o_ptr, OD_NAME_ONLY);
