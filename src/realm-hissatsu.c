41b78786fa18877592f43e32d3add707fb842b4d
Eric Branlund <ebranlund@fastmail.com>
2019-09-26 09:11:19 -0700

    Changed another English spell description involving sleep.

--- src/realm-hissatsu.c
+++ src/realm-hissatsu.c
@@ -726,7 +726,7 @@ concptr do_hissatsu_spell(SPELL_IDX spell, BIT_FLAGS mode)
 
 	case 25:
 		if (name) return _("幻惑", "Moon Dazzling");
-		if (desc) return _("視界内の起きている全モンスターに朦朧、混乱、眠りを与えようとする。", "Attempts to stun, confuse and sleep all waking monsters.");
+		if (desc) return _("視界内の起きている全モンスターに朦朧、混乱、眠りを与えようとする。", "Attempts to stun, confuse and put to sleep all waking monsters.");
 
 		if (cast)
 		{
