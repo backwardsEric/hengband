d5a8c9ebb5b2cca7bbba442a96ed15cd67a31b62
Eric Branlund <ebranlund@fastmail.com>
2019-09-25 21:53:14 -0700

    Changed English message for time-caused experience loss to hopefully be more idiomatic and expressive.

--- src/spells1.c
+++ src/spells1.c
@@ -4434,7 +4434,7 @@ static bool project_p(MONSTER_IDX who, concptr who_name, int r, POSITION y, POSI
 					case 1: case 2: case 3: case 4: case 5:
 					{
 						if (p_ptr->prace == RACE_ANDROID) break;
-						msg_print(_("人生が逆戻りした気がする。", "You feel life has clocked back."));
+						msg_print(_("人生が逆戻りした気がする。", "You feel like a chunk of the past has been ripped away."));
 						lose_exp(100 + (p_ptr->exp / 100) * MON_DRAIN_LIFE);
 						break;
 					}
