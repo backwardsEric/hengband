ddaa37b87944a0bffe1bd794b508d8019cf8f5af
Eric Branlund <ebranlund@fastmail.com>
2019-09-26 13:41:37 -0700

    Combined sentences in English description of "Rock Smash".  Changed preposition to make the description more idiomatic.

--- src/realm-hissatsu.c
+++ src/realm-hissatsu.c
@@ -344,7 +344,7 @@ concptr do_hissatsu_spell(SPELL_IDX spell, BIT_FLAGS mode)
 
 	case 12:
 		if (name) return _("破岩斬", "Rock Smash");
-		if (desc) return _("岩を壊し、岩石系のモンスターに大ダメージを与える。", "Breaks rock. Or greatly damages a monster made by rocks.");
+		if (desc) return _("岩を壊し、岩石系のモンスターに大ダメージを与える。", "Breaks rock or greatly damages a monster made of rocks.");
 
 		if (cast)
 		{
