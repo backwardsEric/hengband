5c2a01723e80e89ae37ee1bb44d77b564d2af968
Eric Branlund <ebranlund@fastmail.com>
2019-12-15 20:06:21 -0800

    To match what's done in other spell descriptions, removed comma before "and" in a list of effects in a spell description.

--- src/realm-crusade.c
+++ src/realm-crusade.c
@@ -247,7 +247,7 @@ concptr do_crusade_spell(SPELL_IDX spell, BIT_FLAGS mode)
 	case 15:
 		if (name) return _("聖なる御言葉", "Holy Word");
 		if (desc) return _("視界内の邪悪な存在に大きなダメージを与え、体力を回復し、毒、恐怖、朦朧状態、負傷から全快する。",
-			"Damages all evil monsters in sight, heals HP somewhat, and completely cures fear, poisons, cuts, and being stunned.");
+			"Damages all evil monsters in sight, heals HP somewhat and completely cures fear, poisons, cuts and being stunned.");
 
 		{
 			int dam_sides = plev * 6;
