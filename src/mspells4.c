116b2e6a132f854225a66d8ec3a194ef5d13cca0
Eric Branlund <ebranlund@fastmail.com>
2019-12-27 19:54:29 -0800

    Removed extra parentheses to avoid a compiler warning from the C compiler included with Apple's XTools (clang version 11.0.0).

--- src/mspells4.c
+++ src/mspells4.c
@@ -217,7 +217,7 @@ void spell_RF4_DISPEL(MONSTER_IDX m_idx, MONSTER_IDX t_idx, int TARGET_TYPE)
 
 		if ((p_ptr->pseikaku == SEIKAKU_COMBAT) || (inventory[INVEN_BOW].name1 == ART_CRIMSON))
 			msg_print(_("やりやがったな！", ""));
-		else if ((p_ptr->pseikaku == SEIKAKU_CHARGEMAN))
+		else if (p_ptr->pseikaku == SEIKAKU_CHARGEMAN)
 		{
 			if (randint0(2) == 0) msg_print(_("ジュラル星人め！", ""));
 			else msg_print(_("弱い者いじめは止めるんだ！", ""));
@@ -2269,7 +2269,7 @@ void spell_RF6_TELE_AWAY(MONSTER_IDX m_idx, MONSTER_IDX t_idx, int TARGET_TYPE)
 	{
 		if ((p_ptr->pseikaku == SEIKAKU_COMBAT) || (inventory[INVEN_BOW].name1 == ART_CRIMSON))
 			msg_print(_("くっそ～", ""));
-		else if ((p_ptr->pseikaku == SEIKAKU_CHARGEMAN))
+		else if (p_ptr->pseikaku == SEIKAKU_CHARGEMAN)
 		{
 			if (randint0(2) == 0) msg_print(_("ジュラル星人め！", ""));
 			else msg_print(_("弱い者いじめは止めるんだ！", ""));
