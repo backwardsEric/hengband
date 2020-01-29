6488580bbc5393555d91a1d133818c1e7325be25
Eric Branlund <ebranlund@fastmail.com>
2020-01-29 01:21:23 -0800

    Modified so save menu item is disabled while on the splash screen.

--- src/main-cocoa.m
+++ src/main-cocoa.m
@@ -4124,7 +4124,8 @@ static void hook_quit(const char * str)
 	[menuItem setState: ((is_on) ? NSOnState : NSOffState)];
 	return YES;
     }
-    else if( sel == @selector(sendAngbandCommand:) )
+    else if( sel == @selector(sendAngbandCommand:) ||
+	     sel == @selector(saveGame:) )
     {
         /* we only want to be able to send commands during an active game */
         return !!game_in_progress;
