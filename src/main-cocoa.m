0aa90418d6e7679c84c61cf7a25ef5037b64d09c
Eric Branlund <ebranlund@fastmail.com>
2020-03-09 18:03:19 -0700

    Changed so the auxiliary windows are not floating panels; otherwise they're always in front of the main window, which is annoying.

--- src/main-cocoa.m
+++ src/main-cocoa.m
@@ -2027,11 +2027,14 @@ static __strong NSFont* gDefaultFont = nil;
 	 */
         if ((__bridge AngbandContext*) (angband_term[0]->data) != self)
         {
-	    self.primaryWindow =
+	    NSPanel *panel =
 		[[NSPanel alloc] initWithContentRect:contentRect
 				 styleMask:(styleMask | NSClosableWindowMask |
 					    NSUtilityWindowMask)
 				 backing:NSBackingStoreBuffered defer:YES];
+
+	    panel.floatingPanel = NO;
+	    self.primaryWindow = panel;
         } else {
 	    self.primaryWindow =
 		[[NSWindow alloc] initWithContentRect:contentRect
