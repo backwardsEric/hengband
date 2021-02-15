9853560ac40c9b35c4bdee63c40199e40344fa1d
Eric Branlund <ebranlund@fastmail.com>
2021-02-15 14:40:37 -0800

    Change to past tense for English message after a successful life rate change.

--- src/spells-status.c
+++ src/spells-status.c
@@ -248,7 +248,7 @@ void roll_hitdice(player_type *creature_ptr, SPOP_FLAGS options)
 		}
 		else
 		{
-			msg_print(_("体力ランクが変わった。", "Life rate is changed."));
+			msg_print(_("体力ランクが変わった。", "Life rate has changed."));
 			creature_ptr->knowledge &= ~(KNOW_HPRATE);
 		}
 	}
