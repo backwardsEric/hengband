9a8c81219dcd9fbe2cf6051b09f1757475bd2c30
Eric Branlund <ebranlund@fastmail.com>
2025-08-13 13:19:06 -0600

    doxygen: use a recognized list format for the steps to generate a .nib file

--- src/cocoa/AppDelegate.m
+++ src/cocoa/AppDelegate.m
@@ -5,29 +5,29 @@
  * Use this file to rebuild the .nib file with Xcode without having to pull
  * in all of the Hengband source.  This is the procedure with Xcode 14:
  *
- * 1) Create a new Xcode project for a macOS App.
- * 2) You can set the "Product Name", "Team", "Organization Name",
+ * 1. Create a new Xcode project for a macOS App.
+ * 2. You can set the "Product Name", "Team", "Organization Name",
  *    "Organization Identifier" as you wish.  Setting the product name to
  *    "hengband" and the "Organization Identifier" to "jp.osdn" will match
  *    the bundle identifier used in the full builds for Hengband.  Set
  *    "Language" to "Objective-C" and "User Interface" to "XIB".  Leave
  *    "Use Core Data" off.  The setting for "Include Tests" doesn't matter;
  *    you can turn it off to avoid extra clutter.
- * 3) In hengband's project settings on the "Info" tab, set the deployment
+ * 3. In hengband's project settings on the "Info" tab, set the deployment
  *    target to what's used in Hengband's src/Makefile.am.  When this was
  *    written, that was 10.8 and 10.8 is necessary for Base localization.
  *    In the localizations part of that tab, click the '+' and add a Japanese
  *    localization.  That will prompt you for the files involved.  Leave that
  *    as is:  one file, "MainMenu.xib", with Base as the reference language
  *    and localizable strings as the file type.
- * 4) In hengband's targets on the "General" tab, verify that "Main Interface"
+ * 4. In hengband's targets on the "General" tab, verify that "Main Interface"
  *    is MainMenu.
- * 5) Copy src/cocoa/AppDelegate.h and src/cocoa/AppDelegate.m from the
+ * 5. Copy src/cocoa/AppDelegate.h and src/cocoa/AppDelegate.m from the
  *    Hengband source files to the directory in the project with main.m.  Copy
  *    src/cocoa/Base.lproj/MainMenu.xib to the Base.lproj subdirectory of that
  *    directory.  Copy src/cocoa/ja.lproj/MainMenu.strings to the ja.lproj
  *    subdirectory of that directory.
- * 6) (This annoyance seems to have gone away beween Xcode 11 and Xcode 13;
+ * 6. (This annoyance seems to have gone away beween Xcode 11 and Xcode 13;
  *    leaving it here just in case) If you modify MainMenu.xib after copying
  *    it over, you may want to set it so that it can be opened in older
  *    versions of Xcode.  Select it in Xcode, and select one of the things,
@@ -37,7 +37,7 @@
  *    "Latest Xcode" will close the file and save it with the appropriate
  *    flags.  Note that reopening the xib file in Xcode and saving it will
  *    cause the version to revert to the latest Xcode.
- * 7) If you want to change the Japanese strings for the menus, one way to
+ * 7. If you want to change the Japanese strings for the menus, one way to
  *    partly do it in Xcode is to export the localizations:  from the file view
  *    select topmost category ("hengband" with an application icon) and then
  *    select Product->Export Localizations... in Xcode's menu bar.  That
@@ -52,8 +52,8 @@
  *    The result of that will be to regenerate ja.lproj/MainMenu.strings in the
  *    Xcode project files which you can use to replace the version in
  *    src/cocoa/ja.lproj/MainMenu.strings in the Hengband source code.
- * 8) Use Xcode's Product->Build For->Running menu entry to build the project.
- * 9) The generated .nib file for English will be
+ * 8. Use Xcode's Product->Build For->Running menu entry to build the project.
+ * 9. The generated .nib file for English will be
  *    Contents/Resources/Base.lproj/MainMenu.nib in the product directory which
  *    is something like
  *    ~/Library/Developer/Xcode/DerivedData/`product_name`-`some_string`/Build/Products/Debug/`product_name`.app
